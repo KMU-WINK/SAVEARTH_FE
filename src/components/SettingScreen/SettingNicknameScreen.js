fe5dcdf35d9a8239b33a611721d379fbfec8225d
BORI <bohhh_@naver.com>
2022-08-25 21:42:13 +0900

    Setting-Screen-backgroundColor

--- src/components/SettingScreen/SettingNicknameScreen.js
+++ src/components/SettingScreen/SettingNicknameScreen.js
@@ -15,6 +15,8 @@ export const SettingNicknameScreen=({navigation})=>{
 
 const styles = StyleSheet.create({
     container:{
+        flex: 1,
+        backgroundColor: '#fff',
         alignItems: 'center'
     }
 })
