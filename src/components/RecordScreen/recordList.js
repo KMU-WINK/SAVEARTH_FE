bae7354345856250080312707479765252f3a275
jny384 <66296986+jny384@users.noreply.github.com>
2022-08-27 00:13:23 +0900

    record-components
    조깅 기록 페이지 프론트
    - 조깅한 날짜와 시간, 운동 시간, 걸음 수, 이동 거리를 보여주는 컴포넌트를 만듦
    - 스크롤뷰
    
    > 추후 백엔드 연동 시 조깅 기록을 불러올 때마다 해당 컴포넌트에 데이터를 적용하여 불어올 예정

---
+++ src/components/RecordScreen/recordList.js
@@ -0,0 +1,28 @@
+import {RecordComponent} from  './recordComponent';
+import {StyleSheet, SafeAreaView, Text, View} from 'react-native';
+
+export const RecordList = () => {
+    return <View style={styles.record}>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+        <RecordComponent/>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    record: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+
+});
